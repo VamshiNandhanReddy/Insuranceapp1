436845d8b42cf92bd74535c219a15fe01298a09c
VamshiNandhanReddy <134676863+VamshiNandhanReddy@users.noreply.github.com>
2024-05-18 00:44:36 +0530

    Update Insuranceapp.py

--- Insuranceapp.py
+++ Insuranceapp.py
@@ -1,1 +1,18 @@
+# streamlit_app.py
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
 
+st.title('Simple Linear Regression App')
+
+# Load the trained model
+with open('linear_regression_model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+# User input
+income_level = st.number_input('Enter Income Level:', min_value=0, step=1)
+
+if st.button('Predict'):
+    prediction = model.predict(np.array([[income_level]]))
+    st.write(f'Predicted Risk: {prediction[0]}')
